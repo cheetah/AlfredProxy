f4661c8ff2ed57a3c5ac88b292fd01a87d1a28d8
cheetah <thcheetah@gmail.com>
2011-12-23 00:44:04 +0600

    Base functionality done.

--- proxy.sh
+++ proxy.sh
@@ -3,13 +3,31 @@
 case $1 in
   "on")  
     networksetup -setwebproxystate wi-fi on
-    echo "Proxy is on."
+    echo -n "Turning on proxy"
     ;;
+  
   "off")
     networksetup -setwebproxystate wi-fi off
-    echo "Proxy is off."
+    echo -n "Turning off proxy"
     ;;
+  
+  "toggle")
+    e=$(networksetup -getwebproxy wi-fi | grep "No")
+    
+    if [ -n "$e" ]; then
+      networksetup -setwebproxystate wi-fi on
+      echo -n "Turning on proxy"
+    else
+      networksetup -setwebproxystate wi-fi off
+      echo -n "Turning off proxy"
+    fi
+    ;;
+  
+  "status")
+    networksetup -getwebproxy wi-fi
+    ;;
+  
   *)
-    echo "Wrong action."
+    echo -n "Wrong action"
     ;;
 esac
